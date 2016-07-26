0de247bbc33224f1ab0dd3486f825c8f93f560b3
Tom Leskin <tleskin@gmail.com>
2016-07-25 23:49:34 -0400

    Add templates for navbar and index, add posts

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
+from flask import render_template
 from app import app
-
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Tom'}
+    posts = [
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Florida!' 
+        },
+        { 
+            'author': {'nickname': 'Dalton'}, 
+            'body': 'I like turtles!' 
+        },
+        { 
+            'author': {'nickname': 'Tom'}, 
+            'body': 'I think I like Python and Flask!' 
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
